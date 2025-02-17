4b88df7963aa09498b4bf2fd58db0c28b159b62f
davidmrc6 <mrcdavid6@gmail.com>
2025-02-17 11:52:33 +0100

    Testing

--- src/modules/Onboarding/pages/ApplyView.vue
+++ src/modules/Onboarding/pages/ApplyView.vue
@@ -10,7 +10,13 @@
     const step = ref(1);
     const total = ref(6);
 
-    type Answer = {
+    interface Question {
+      question: string;
+      type: string;
+      key: keyof Answer;
+    }
+
+    interface Answer {
         handle: string,
         based_in: string,
         date_of_birth: string,
@@ -19,49 +25,50 @@
         tiktok: string,
     }
 
-    const questions: { question: string, type: string }[] = [
+    const questions: Question[] = [
         {
-            question: 'first what is your ig handle?',
+            question: 'first, what is your instagram handle?',
             type: 'text',
+            key: 'handle',
         },
         {
-            question: 'alright thanks, now where are you based?',
+            question: 'alright, thanks. now, where are you based?',
             type: 'text',
+            key: 'based_in',
         },
         {
             question: 'great! when were you born?',
             type: 'date',
+            key: 'date_of_birth',
         },
         {
             question: 'what project types do you mostly work on?',
             type: 'multi',
+            key: 'project_types',
         },
         {
             question: 'have anything else you want to share with us?',
             type: 'textarea',
+            key: 'additional_info',
         },
         {
             question: 'do you also have a tiktok account?',
             type: 'text',
+            key: 'tiktok',
         }
     ]
 
-    const answers: Answer = {
+    const answers = reactive<Answer>({
         handle: '',
         based_in: '',
         date_of_birth: '',
         additional_info: '',
         project_types: '',
         tiktok: '',
-    }
+    })
 
-    async function next() {
-        step.value++;
-    }
-
-    async function back() {
-        step.value--;
-    }
+    const next = () => step.value ++;
+    const back = () => step.value --;
 
     async function finished() {
         if(!accountState.user) return;
@@ -74,9 +81,6 @@
         });
 
         accountState.user.status = AccountStatus.IN_REVIEW;
-
-
-
         await router.push('/')
     }
 
@@ -95,19 +99,26 @@
         <ProgressBar :step :total/>
         <div class="flex flex-grow justify-center px-6">
             <div class="w-[850px] max-w-full mt-20 ">
-                <OnboardingQuestion v-model="answers.handle" v-if="step == 1" :question="questions[0]" :step :total
-                                    @next="next" @back="back"></OnboardingQuestion>
-                <OnboardingQuestion v-model="answers.based_in" v-if="step == 2" :question="questions[1]" :step :total
-                                    @next="next" @back="back"></OnboardingQuestion>
-                <OnboardingQuestion v-model="answers.date_of_birth" v-if="step == 3" :question="questions[2]" :step
-                                    :total @next="next" @back="back"></OnboardingQuestion>
-                <OnboardingQuestion v-model="answers.project_types" v-if="step == 4" :question="questions[3]" :step
-                                    :total @next="next" @back="back"></OnboardingQuestion>
-                <OnboardingQuestion v-model="answers.additional_info" v-if="step == 5" :question="questions[4]" :step
-                                    :total @next="next" @back="back"></OnboardingQuestion>
-                <OnboardingQuestion v-model="answers.tiktok" v-if="step == 6" :question="questions[5]" :step :total
-                                    @next="next" @back="back"></OnboardingQuestion>
-                <button v-if="step == total" @click="finished" class="bg-black text-white px-24 py-3 rounded-lg mt-6">
+
+              <!-- could probably simplify this -->
+              <!-- also, keep track of application state maybe, so if user goes back to previous question, his answer is saved -->
+              <OnboardingQuestion v-model="answers.handle" v-if="step == 1" :question="questions[0]" :step :total
+                                  @next="next" @back="back"></OnboardingQuestion>
+              <OnboardingQuestion v-model="answers.based_in" v-if="step == 2" :question="questions[1]" :step :total
+                                  @next="next" @back="back"></OnboardingQuestion>
+              <OnboardingQuestion v-model="answers.date_of_birth" v-if="step == 3" :question="questions[2]" :step
+                                  :total @next="next" @back="back"></OnboardingQuestion>
+              <OnboardingQuestion v-model="answers.project_types" v-if="step == 4" :question="questions[3]" :step
+                                  :total @next="next" @back="back"></OnboardingQuestion>
+              <OnboardingQuestion v-model="answers.additional_info" v-if="step == 5" :question="questions[4]" :step
+                                  :total @next="next" @back="back"></OnboardingQuestion>
+              <OnboardingQuestion v-model="answers.tiktok" v-if="step == 6" :question="questions[5]" :step :total
+                                  @next="next" @back="back"></OnboardingQuestion>
+                <button
+                    v-if="step == total"
+                    @click="finished"
+                    class="bg-black text-white px-24 py-3 rounded-lg mt-6"
+                >
                     finish
                 </button>
             </div>
