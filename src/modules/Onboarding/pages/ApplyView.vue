4a8e9c4b25c8d68bdd8d48fa6b13b276225741d6
davidmrc6 <mrcdavid6@gmail.com>
2025-03-09 13:10:42 +0200

    Fixed organization in ApplyView template

--- src/modules/Onboarding/pages/ApplyView.vue
+++ src/modules/Onboarding/pages/ApplyView.vue
@@ -58,22 +58,20 @@
 
 <template>
 	<section class="flex screen-size flex-col pb-10">
-		<!-------------------------------------------------------------------------------->
-		<!-- navbar -->
+		<!-- Navigation Bar -->
 		<nav
 			class="relative w-full flex items-center text-center px-12 py-6 justify-center"
 		>
-			<!-- go back button -->
-			<!-- Desktop version -->
+			<!-- Back Button - Desktop -->
 			<button
 				class="absolute left-[15%] hidden lg:block"
 				:disabled="!onboardingStore.canGoBack"
 				@click="onboardingStore.back"
 			>
-				back
+				{{ onboardingStore.cameFromReview ? "back to review" : "back" }}
 			</button>
 
-			<!-- Mobile version -->
+			<!-- Back Button - Mobile -->
 			<button
 				class="absolute left-4 block lg:hidden"
 				:disabled="!onboardingStore.canGoBack"
@@ -82,60 +80,59 @@
 				<img src="/icons/arrow-back.svg" alt="back" class="w-4 h-4" />
 			</button>
 
-			<!-- wrapper for logo and progress indicator -->
+			<!-- Logo and Progress Container -->
 			<div class="flex flex-col items-center">
-				<!-- creatormate logo - desktop -->
+				<!-- Logo - Desktop -->
 				<img
+					src="/creatormate.svg"
 					alt="creatormate-logo"
 					class="h-[15.134px] w-[128px] hidden lg:block"
-					src="/creatormate.svg"
 				/>
-				<!-- logo for mobile -->
+				<!-- Logo - Mobile -->
 				<img
+					src="/logo.png"
 					alt="cm"
 					class="h-[10px] w-[26.6px] block lg:hidden"
-					src="/logo.png"
 				/>
 			</div>
 
-			<!-- Action buttons container -->
+			<!-- Action Buttons - Desktop -->
 			<div class="absolute right-[15%] hidden lg:flex space-x-2">
-				<!-- save state button - desktop -->
 				<button
 					class="flex items-center gap-2 px-5 py-2 hover:bg-[#E9E9E9] rounded-lg transition-all duration-150"
 				>
 					save & exit
 				</button>
 
-				<!-- logout button - desktop -->
 				<button
 					class="flex items-center gap-2 px-8 py-2 bg-gray-100 hover:bg-[#E9E9E9] rounded-lg transition-all duration-150"
 					@click="logout()"
 				>
 					<span>logout</span>
-					<img src="/icons/logout.svg" alt="" class="w-4 h-4 text-gray-400" />
+					<img
+						src="/icons/logout.svg"
+						alt="logout"
+						class="w-4 h-4 text-gray-400"
+					/>
 				</button>
 			</div>
 
-			<!-- Mobile buttons container -->
+			<!-- Action Buttons - Mobile -->
 			<div class="absolute right-4 flex items-center space-x-2 lg:hidden">
-				<!-- save state button - mobile -->
 				<button class="flex items-center text-sm gap-2 px-3 py-2 rounded-lg">
 					save & exit
 				</button>
 
-				<!-- logout button - mobile -->
 				<button
 					class="flex items-center bg-gray-100 p-2 rounded-lg transition-all duration-150"
 					@click="logout()"
 				>
-					<img src="/icons/logout.svg" alt="Logout" class="w-4 h-4" />
+					<img src="/icons/logout.svg" alt="logout" class="w-4 h-4" />
 				</button>
 			</div>
 		</nav>
-		<!-------------------------------------------------------------------------------->
 
-		<!-- progress indicator -->
+		<!-- Progress Indicator -->
 		<div class="flex justify-center w-full mt-3 lg:mt-[-5px]">
 			<ProgressIndicator
 				:step="onboardingStore.currentStep"
@@ -144,6 +141,7 @@
 			/>
 		</div>
 
+		<!-- Main Content -->
 		<div
 			v-if="isLoading"
 			class="flex items-center justify-center h-[calc(100vh-68px)]"
@@ -154,31 +152,32 @@
 		<div v-else>
 			<div class="relative flex flex-grow justify-center px-6">
 				<div class="w-[636px] max-w-full mt-20 gap-5">
+					<!-- Error Message -->
 					<span v-if="onboardingStore.errorMessage !== ''" class="text-red-600">
 						{{ onboardingStore.errorMessage }}
 					</span>
 
+					<!-- Content Components -->
 					<ApplicationReview
-						class="mb-16"
 						v-if="onboardingStore.isReviewStep"
+						class="mb-16"
 					/>
 
-					<!-- TODO: add transition between questions -->
 					<QuestionRenderer
-						class="mb-16"
 						v-else-if="onboardingStore.currentQuestion"
 						:question="onboardingStore.currentQuestion"
+						class="mb-16"
 					/>
 				</div>
 			</div>
 		</div>
 
-		<!-- next button -->
-		<!-- desktop version -->
+		<!-- Navigation Buttons -->
+		<!-- Next Button - Desktop -->
 		<div class="hidden md:block">
 			<button
-				class="fixed bottom-4 right-[15%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400 hover:bg-[#242424] transition-all duration-150"
 				v-if="!onboardingStore.isLastStep"
+				class="fixed bottom-4 right-[15%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400 hover:bg-[#242424] transition-all duration-150"
 				@click="onboardingStore.next"
 				:disabled="!onboardingStore.canProceed"
 			>
@@ -186,11 +185,11 @@
 			</button>
 		</div>
 
-		<!-- mobile version -->
+		<!-- Next Button - Mobile -->
 		<div class="md:hidden">
 			<button
-				class="fixed bottom-4 mx-auto left-0 right-0 w-[95%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400 hover:bg-[#242424] transition-all duration-150"
 				v-if="!onboardingStore.isLastStep"
+				class="fixed bottom-4 mx-auto left-0 right-0 w-[95%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400 hover:bg-[#242424] transition-all duration-150"
 				@click="onboardingStore.next"
 				:disabled="!onboardingStore.canProceed"
 			>
@@ -198,26 +197,25 @@
 			</button>
 		</div>
 
-		<!-- submit button -->
-		<!-- desktop version -->
+		<!-- Submit Button - Desktop -->
 		<div class="hidden md:block">
 			<button
-				class="fixed bottom-4 right-[15%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400"
 				v-if="onboardingStore.isReviewStep"
-				:disabled="!onboardingStore.isTOSAccepted"
+				class="fixed bottom-4 right-[15%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400 hover:bg-[#242424] transition-all duration-150"
 				@click="submitApplication()"
+				:disabled="!onboardingStore.isTOSAccepted"
 			>
 				submit application
 			</button>
 		</div>
 
-		<!-- mobile version -->
+		<!-- Submit Button - Mobile -->
 		<div class="md:hidden">
 			<button
-				class="fixed bottom-4 mx-auto left-0 right-0 w-[95%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400 hover:bg-[#242424] transition-all duration-150"
 				v-if="onboardingStore.isReviewStep"
-				:disabled="!onboardingStore.isTOSAccepted"
+				class="fixed bottom-4 mx-auto left-0 right-0 w-[95%] bg-black text-white px-5 py-2 rounded-lg mt-6 disabled:bg-gray-400 hover:bg-[#242424] transition-all duration-150"
 				@click="submitApplication()"
+				:disabled="!onboardingStore.isTOSAccepted"
 			>
 				submit application
 			</button>
