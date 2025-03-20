87e3c6731127e2ddcd57b22236976ae6ef80cad2
Robin Mons <robin.mons@hotmail.nl>
2025-03-20 16:55:44 +0100

    added user fallback creation

--- src/api/modules/users/GetSelf/GetSelfEndpoint.ts
+++ src/api/modules/users/GetSelf/GetSelfEndpoint.ts
@@ -3,6 +3,7 @@ import type {Context} from "hono";
 import type {HonoUser} from "~/src/api/utils/HonoComposables";
 import {errorResponse, successResponse} from "~/src/api/utils/HonoResponses";
 import hono from "~/src/api/hono";
+import type {User} from "@supabase/auth-js";
 
 export class GetSelfEndpoint extends Endpoint {
     protected readonly method: string = 'get'
@@ -12,12 +13,25 @@ export class GetSelfEndpoint extends Endpoint {
         const honoUser = this.getHonoUser(context);
         let user = await this.getUser(honoUser.id);
 
-        if(!user) return errorResponse(context, 'USER_NOT_FOUND');
+        if(!user) {
+            await this.createUser(honoUser);
+            user = await this.getUser(honoUser.id);
+        }
 
         if (!user) return errorResponse(context, "User not found or could not be created");
 
         return successResponse(context, user);
     }
+
+    private async createUser(honoUser: User) {
+        await this.prismaClient.users.create({
+            data: {
+                id: honoUser.id,
+                email: honoUser.email ?? '',
+            }
+        });
+    }
+
     private async getUser(id: string) {
         return this.prismaClient.users.findUnique({
             where: {id: id},
